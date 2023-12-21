80b9fddeaa23b05ead656cdd54abecf6c61c56c3
ducpm511 <phamminhduc0511@gmail.com>
2023-12-21 21:44:20 +0700

    - added mobile menu and homepage carousel

--- svg-gallery/src/components/Header/Header.jsx
+++ svg-gallery/src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
+import {useState} from "react";
 import "./style.scss";
 import NavigationBar from "../NavigationBar/navigationBar";
 import LogoContainer from "./logo";
 import MenuIcon from "@mui/icons-material/Menu";
+import MobileMenu from "../NavigationBar/mobileMenu";
+import CloseIcon from '@mui/icons-material/Close';
 const Header = () => {
+  const [openMobileMene, setOpenMobileMenu] = useState(false);
+
+  const mobileMenuIconClicked = () => {
+    setOpenMobileMenu(!openMobileMene);
+  }
   return (
     <div className={"header"}>
       <div className={"container mx-auto headerContainer"}>
@@ -11,11 +19,17 @@ const Header = () => {
       </div>
       <div className={"container mx-auto mobileHeader"}>
         <div className={"headerLeft"}>
-          <MenuIcon />
+          {
+            openMobileMene? 
+            <CloseIcon onClick={mobileMenuIconClicked}/>
+            :
+            <MenuIcon onClick={mobileMenuIconClicked}/>
+          }
           <LogoContainer />
+          <MobileMenu isOpen={openMobileMene}/>
         </div>
-
         <NavigationBar />
+
       </div>
     </div>
   );
