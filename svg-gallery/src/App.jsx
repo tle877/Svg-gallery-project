ccf127a2440364eca309fd10f1c7f5339883c8a0
tle877 <jackle_93@yahoo.com>
2023-12-27 23:18:28 -0800

    Upate Production Step

--- svg-gallery/src/App.jsx
+++ svg-gallery/src/App.jsx
@@ -5,7 +5,7 @@ import AppRoutes from "./Routes";
 import AnnouncementBar from "./components/AnnouncementBar/announcementBar";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
-
+import ProductionStep from "./components/ProductionStep/ProductionStep";
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
       <AnnouncementBar />
       <Header/>
       <AppRoutes />
+      <ProductionStep />
       <Footer />
     </>
   );
