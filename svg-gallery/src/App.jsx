bd06e9f7c5ea937c61a89b37d3de73445bf29498
tle877 <jackle_93@yahoo.com>
2024-01-02 22:21:47 -0800

    Update Tags DropDown

--- svg-gallery/src/App.jsx
+++ svg-gallery/src/App.jsx
@@ -6,6 +6,7 @@ import AppRoutes from "./Routes";
 import AnnouncementBar from "./components/AnnouncementBar/announcementBar";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
+import { TagsDropdown } from "./components/TagsDropdown/TagsDropdown";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <AnnouncementBar />
       <Header />
       <AppRoutes />
+      <TagsDropdown />
       <Footer />
     </>
   );
---
+++ svg-gallery/src/components/TagsDropdown/tagList.js
@@ -0,0 +1,66 @@
+export const tagList = [
+    {
+      title: "Tags",
+      handle: "",
+    },
+    {
+      title: "Svg",
+      handle: "",
+    },
+    {
+      title: "Svg",
+      handle: "",
+    },
+    {
+      title: "Home Decor",
+      handle: "",
+    },
+    {
+      title: "Christmas",
+      handle: "",
+    },
+    {
+      title: "Home Decor",
+      handle: "",
+    },
+    {
+      title: "Tags",
+      handle: "",
+    },
+    {
+      title: "Christmas",
+      handle: "",
+    },
+    {
+      title: "Tags",
+      handle: "",
+    },
+    {
+      title: "Svg",
+      handle: "",
+    },
+    {
+      title: "Home Decor",
+      handle: "",
+    },
+    {
+      title: "Christmas",
+      handle: "",
+    },
+    {
+      title: "Tags",
+      handle: "",
+    },
+    {
+      title: "Svg",
+      handle: "",
+    },
+    {
+      title: "Home Decor",
+      handle: "",
+    },
+    {
+      title: "Christmas",
+      handle: "",
+    },
+  ];
